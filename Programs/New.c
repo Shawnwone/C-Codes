4f374d7f683fab41426a3bb6d6ab159fe6b6ad00
ShawnWone <shawnwone@outlook.com>
2025-11-01 01:25:44 +0800

    Test_2

--- Programs/New.c
+++ Programs/New.c
@@ -1,9 +1,5 @@
 #include<stdio.h>
 int main(){
-    int a=0;
-    printf("%d\n", a);
-    a = 5;
-    scanf("%d", &a);
-    printf("%d\n", a);
-    return 0;
+    printf("Hello World");
+    return 0; 
 }
